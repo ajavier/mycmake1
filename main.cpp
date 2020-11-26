1b20a35b70efbdb94dae26a164338fcf378f03ff
Javier Albornoz <albjavier@gmail.com>
2020-11-25 21:28:56 -0600

    Initial commit for the test repo
    Signed-off-by: Javier Albornoz <albjavier@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int, char**) {
+    int edad=46;
+    std::string name="Javier";
+
+    std::cout << name << " is " << edad << " years-old" << std::endl;
+
+    return EXIT_SUCCESS;
+}
